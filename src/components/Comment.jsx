e345963dae7c2dd72580293cf337131c25dcb178
Gabriel Alerrandro <gabrielalerrandro081@gmail.com>
2024-08-27 11:34:46 -0300

    dwa,kjb

---
+++ src/components/Comment.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react"
+import { Avatar } from "./Avatar"
+import styles from "./Comment.module.css"
+import { ThumbsUp, Trash } from "phosphor-react"
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/GabrielAlerrandro.png"
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Autor</strong>
+              <time
+                title="15 de Agosto às 14:48h"
+                dateTime="2024-08-15 14:48:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
